b47d5168ef9748c15f9f8d7f46083ec29f577a10
Radek Pleskac <info@radekpleskac.com>
2023-03-14 17:02:54 +0100

    add tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,14 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className="text-xl">Welcome to Remix</h1>
       <ul>
         <li>
           <a
             target="_blank"
             href="https://remix.run/tutorials/blog"
             rel="noreferrer"
+            className="text-green-800"
           >
             15m Quickstart Blog Tutorial
           </a>
@@ -17,12 +18,18 @@ export default function Index() {
             target="_blank"
             href="https://remix.run/tutorials/jokes"
             rel="noreferrer"
+            className="text-green-800"
           >
             Deep Dive Jokes App Tutorial
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+          <a
+            target="_blank"
+            href="https://remix.run/docs"
+            rel="noreferrer"
+            className="text-green-800"
+          >
             Remix Docs
           </a>
         </li>
