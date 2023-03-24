96e01e342e95c61077200beeff9e60f384df37b7
Radek Pleskac <info@radekpleskac.com>
2023-03-24 21:56:52 +0100

    simple project detail page

--- app/models/projects.server.ts
+++ app/models/projects.server.ts
@@ -1,4 +1,16 @@
 import { prisma } from '~/db.server'
+import { type Project } from 'prisma/prisma-client'
+
+export async function getProject(slug: Project['slug']) {
+  return await prisma.project.findUnique({
+    where: {
+      slug,
+    },
+    include: {
+      images: true,
+    },
+  })
+}
 
 export async function getProjects() {
   return await prisma.project.findMany({
--- app/routes/projects.$slug.tsx
+++ app/routes/projects.$slug.tsx
@@ -1,19 +1,38 @@
-import { useParams } from '@remix-run/react'
+import { json, type LoaderArgs } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { getProject } from '~/models/projects.server'
+
 import Header from '~/components/Header'
 
+export async function loader({ params }: LoaderArgs) {
+  return json({ project: await getProject(params.slug || '') })
+}
+
 export default function ProjectSlug() {
-  const params = useParams()
-  const slug = params.slug
-  console.log('CLOG ~ ProjectSlug ~ slug:', slug)
+  const { project } = useLoaderData<typeof loader>()
+
   return (
     <>
       <Header />
-      <div className="min-h-screen">
-        <main className="container md:min-h-screen px-6 sm:px-6 py-6 sm:py-10 lg:py-14 mb-20 mx-auto max-w-4xl">
-          <h1 className="pb-6 text-5xl font-bold">Project detail for</h1>
-          <h2>{slug}</h2>
-        </main>
-      </div>
+      {project ? (
+        <div className="min-h-screen">
+          <main className="container md:min-h-screen px-6 sm:px-6 py-6 sm:py-10 lg:py-14 mb-20 mx-auto max-w-4xl">
+            <h1 className="pb-6 text-5xl font-bold">{project?.title}</h1>
+            <img
+              className="md:max-w-sm"
+              src={`/images/${project.images[0].fileName}`}
+              alt={project.title}
+            />
+            <h3>{project?.description}</h3>
+            <p>{project?.content}</p>
+            <Link className="text-base text-[#fca311]" to={`/projects`}>
+              All projects
+            </Link>
+          </main>
+        </div>
+      ) : (
+        <div>Project does not exist</div>
+      )}
     </>
   )
 }
