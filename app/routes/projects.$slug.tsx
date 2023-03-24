1505f4aad66b476f6b1d57fce61a915188f3f389
Radek Pleskac <info@radekpleskac.com>
2023-03-24 22:15:49 +0100

    - add url to project model - a bit of spacing

--- app/routes/projects.$slug.tsx
+++ app/routes/projects.$slug.tsx
@@ -17,14 +17,23 @@ export default function ProjectSlug() {
       {project ? (
         <div className="min-h-screen">
           <main className="container md:min-h-screen px-6 sm:px-6 py-6 sm:py-10 lg:py-14 mb-20 mx-auto max-w-4xl">
-            <h1 className="pb-6 text-5xl font-bold">{project?.title}</h1>
+            <h1 className="text-5xl font-bold">{project?.title}</h1>
+            <h3 className="mt-3">{project?.description}</h3>
+            {project.url ? (
+              <Link
+                className="text-base text-[#fca311] inline-block my-4"
+                to={project.url}
+                target="_blank"
+              >
+                {project.url}
+              </Link>
+            ) : null}
             <img
               className="md:max-w-sm"
               src={`/images/${project.images[0].fileName}`}
               alt={project.title}
             />
-            <h3>{project?.description}</h3>
-            <p>{project?.content}</p>
+            <p className="my-3">{project?.content}</p>
             <Link className="text-base text-[#fca311]" to={`/projects`}>
               All projects
             </Link>
